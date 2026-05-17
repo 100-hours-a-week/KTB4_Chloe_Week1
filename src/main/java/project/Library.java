0e9e9d1868cd2a744398b73efe3dcf01a50a0412
yewonida <seoyaewon@naver.com>
2026-05-17 19:40:57 +0900

    feat:도서의 분류에 따른 목록 보여주기

--- src/main/java/project/Library.java
+++ src/main/java/project/Library.java
@@ -7,6 +7,11 @@ import java.util.Scanner;
 public class Library {
 
     static String LibraryCode = "1234";
+    static LiteratureBook[] literatureBooks;
+    static EconomyBook[] economyBooks;
+    static ScienceBook[] scienceBooks;
+    static WorkBook[] workBooks;
+    static CartoonBook[] cartoonBooks;
 
     public static void VerifyCode(){
         Scanner scanner = new Scanner(System.in);
@@ -22,28 +27,30 @@ public class Library {
         }
     }
 
-    public static void BookListView(){
-        LiteratureBook[] literatureBooks = new LiteratureBook[2];
+    public static void BookListCreate(){
+        literatureBooks = new LiteratureBook[2];
         literatureBooks[0] = new LiteratureBook("젊은 베르테르의 슬픔");
         literatureBooks[1] = new LiteratureBook("문학책2");
 
-
-        EconomyBook[] economyBooks = new EconomyBook[3];
+        economyBooks = new EconomyBook[3];
         economyBooks[0] = new EconomyBook("경제랑 놀자");
         economyBooks[1] = new EconomyBook("경제책2");
         economyBooks[2] = new EconomyBook("경제책3");
 
-        ScienceBook[] scienceBooks = new ScienceBook[2];
+        scienceBooks = new ScienceBook[2];
         scienceBooks[0] = new ScienceBook("과학책1");
         scienceBooks[1] = new ScienceBook("과학책2");
 
-        WorkBook[] workBooks = new WorkBook[1];
+        workBooks = new WorkBook[1];
         workBooks[0] = new WorkBook("문제집1");
 
-        CartoonBook[] cartoonBooks = new CartoonBook[2];
+        cartoonBooks = new CartoonBook[2];
         cartoonBooks[0] = new CartoonBook("만화책1");
         cartoonBooks[1] = new CartoonBook("만화책2");
+    }
 
+    public static void BookListViewForm(){
+        BookListCreate();
         Scanner scanner = new Scanner(System.in);
         String PaperBook = "1.종이책";
         String EBook = "2.전자책";
@@ -51,26 +58,67 @@ public class Library {
         System.out.println(PaperBook);
         System.out.println(EBook);
         System.out.println("번호를 선택해주세요.");
-        int UserSelectNumber = scanner.nextInt();
+        int UserSelectForm = scanner.nextInt();
 
-        if(UserSelectNumber == 1){
-            System.out.println("종이책 목록");
-            literatureBooks[0].BookList();
-            literatureBooks[1].BookList();
-            economyBooks[0].BookList();
-            economyBooks[1].BookList();
-            economyBooks[2].BookList();
-            scienceBooks[0].BookList();
-            scienceBooks[1].BookList();
+        if( UserSelectForm == 1){
+            BookListViewPaper();
         }
-        else if(UserSelectNumber == 2){
-            workBooks[0].BookList();
-            cartoonBooks[0].BookList();
-            cartoonBooks[1].BookList();
+        else if(UserSelectForm == 2){
+            BookListViewElectronic();
         }
         else{
             System.out.println("번호를 다시 선택해주세요.");
-            BookListView();
+            BookListViewForm();
+        }
+    }
+    public static void BookListViewPaper(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("종이책 목록");
+        System.out.println("1.문학");
+        System.out.println("2.경제");
+        System.out.println("2.경제");
+        System.out.println("번호를 선택해주세요.");
+        int UserSelectPaperField = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (UserSelectPaperField){
+            case 1 :
+                literatureBooks[0].BookList();
+                literatureBooks[1].BookList();
+                break;
+            case 2 :
+                economyBooks[0].BookList();
+                economyBooks[1].BookList();
+                economyBooks[2].BookList();
+                break;
+            case 3 :
+                scienceBooks[0].BookList();
+                scienceBooks[1].BookList();
+            default:
+                System.out.println("번호를 다시 선택해주세요.");
+                BookListViewPaper();
+        }
+    }
+
+    public static void BookListViewElectronic(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("전자책 목록");
+        System.out.println("1.문제집");
+        System.out.println("2.만화책");
+        System.out.println("번호를 선택해주세요.");
+        int UserSelectElectronicField = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (UserSelectElectronicField){
+            case 1:
+                workBooks[0].BookList();
+            case 2:
+                cartoonBooks[0].BookList();
+                cartoonBooks[1].BookList();
+            default:
+                System.out.println("번호를 다시 선택해주세요.");
+                BookListViewElectronic();
         }
     }
 
@@ -89,10 +137,9 @@ public class Library {
         scanner.nextLine(); // 남아있는 엔터 제거
 
 
-
         switch ( UserSelectWork ){
             case 1 :
-                BookListView();
+                BookListViewForm();
                 UserSelectWork();
                 break;
             case 2 :
